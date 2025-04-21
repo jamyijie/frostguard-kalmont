8b20d697410671070d5f15be0eafd59c71bf4749
Jamyijie <yijiewang2@my.caoilanou.ca>
2025-04-20 22:18:44 -0700

    make responsive, add menu toggle stuff

--- js/header-dropdown.js
+++ js/header-dropdown.js
@@ -3,3 +3,15 @@ let header = document.querySelector("header");
 menuToggle.onclick = function () {
   header.classList.toggle("active");
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const submenuParents = document.querySelectorAll(".has-submenu > a");
+
+  submenuParents.forEach((parent) => {
+    parent.addEventListener("click", function (e) {
+      e.preventDefault(); // prevent navigation
+      const listItem = this.parentElement;
+      listItem.classList.toggle("open");
+    });
+  });
+});
